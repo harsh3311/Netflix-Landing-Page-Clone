57847764742d9c01215ca5a08cd5db18847cbace
Harsh <harshbhardwajofficial1414@gmail.com>
2024-06-04 21:15:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// ----------------------------------------------------------FAQs List Click Function---------------------------------------------------------------
+
+// Get all Accordion and Panel
+let accHeading = document.querySelectorAll(".question");
+let accPanel = document.querySelectorAll(".answer");
+
+for (let i = 0; i < accHeading.length; i++) {
+    // Execute whenever an accordion is clicked 
+    accHeading[i].onclick = function() {
+        if (this.nextElementSibling.style.maxHeight) {
+           hidePanels();     // Hide All open Panels 
+        } else {
+           showPanel(this);  // Show the panel
+        } 
+    };
+}
+
+// Function to Show a Panel
+function showPanel(element) {
+  hidePanels();
+  element.classList.add("active");
+  element.nextElementSibling.style.maxHeight = element.nextElementSibling.scrollHeight + "px";
+}
+
+// Function to Hide all shown Panels
+function hidePanels() {
+  for (let i = 0; i < accPanel.length; i++) {
+      accPanel[i].style.maxHeight = null;
+      accHeading[i].classList.remove("active");
+  }
+}
