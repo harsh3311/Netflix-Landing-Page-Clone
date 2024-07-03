f2beaf84cd3d570d20613dfc80cb06b2e7ca1785
harsh3311 <harshbhardwajofficial1414@gmail.com>
2024-07-03 16:46:23 +0530

    commit

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ let accHeading = document.querySelectorAll(".question");
 let accPanel = document.querySelectorAll(".answer");
 
 for (let i = 0; i < accHeading.length; i++) {
-    // Execute whenever an accordion is clicked 
-    accHeading[i].onclick = function() {
-        if (this.nextElementSibling.style.maxHeight) {
-           hidePanels();     // Hide All open Panels 
-        } else {
-           showPanel(this);  // Show the panel
-        } 
-    };
+  // Execute whenever an accordion is clicked 
+  accHeading[i].onclick = function () {
+    if (this.nextElementSibling.style.maxHeight) {
+      hidePanels();     // Hide All open Panels 
+    } else {
+      showPanel(this);  // Show the panel
+    }
+  };
 }
 
 // Function to Show a Panel
@@ -25,7 +25,15 @@ function showPanel(element) {
 // Function to Hide all shown Panels
 function hidePanels() {
   for (let i = 0; i < accPanel.length; i++) {
-      accPanel[i].style.maxHeight = null;
-      accHeading[i].classList.remove("active");
+    accPanel[i].style.maxHeight = null;
+    accHeading[i].classList.remove("active");
   }
 }
+
+
+// ----------------------------------------------------------OnSubmit Function---------------------------------------------------------------
+
+document.querySelector('.email-signup').addEventListener('submit', function (event) {
+  event.preventDefault();
+  window.location.href = 'https://www.netflix.com/login';
+});
